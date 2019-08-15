39dc323c9b540e7dac17844d76c8e4ea54bcaefa
SoubhagyaKumaNayak <53268143+SoubhagyaKumaNayak@users.noreply.github.com>
2019-08-15 16:30:05 +0530

    Create Prob 9.cpp

---
+++ Prob 9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    for(a=1;a<=1000;++a)
+    {
+        for(b=a+1;b<=1000;++b)
+        {
+            for(c=a+1;c<=1000;++c)
+            {
+                if((a*a+b*b==c*c)&&(a+b+c==1000))
+                {
+                    cout<<a*b*c;
+                }
+            }
+        }
+    }
+    return 0;
+    
+} 
