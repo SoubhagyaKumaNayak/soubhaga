985d0ae3aa556f1606143ecdf2cb08683b599c1c
SoubhagyaKumaNayak <53268143+SoubhagyaKumaNayak@users.noreply.github.com>
2019-08-25 11:41:31 +0530

    Update Prob 4.cpp

--- Prob 4.cpp
+++ Prob 4.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 int main() {
     int r,s=0;
-    long int num,max=0,i,j;
-    for(i=100;i<=999;++i)
+    long int num,max=0,i;
+    for(i=10000;i<=998001;++i)
     {
-        for(j=100;j<=999;++j)
-        {
+        
+        
             s=0;
-            num=i*j;
+            num=i;
             while(num>0)
             {
                 r=num%10;
                 s=s*10+r;
                 num=num/10;
             }
-            if(s==i*j)
+            if(s==i)
             {
-                if(i*j>max)
-                max=i*j;
+                if(i>max)
+                max=i;
             }
         }
     }
